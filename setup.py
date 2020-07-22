6b5635e77f9cfbc804e4e510b785a6d63244c6cf
patrickryan <theyoungsoul@gmail.com>
2020-07-21 19:49:53 -0500

    getting ready to complete intro video

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='tello_adv_setup',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='patrick ryan',
+    author_email='',
+    description='Tello Advanced Project Development Setup ',
+    install_requires = [
+        'opencv-python==4.2.0.34',
+        'opencv-contrib-python==4.2.0.34',
+        'jupyter==1.0.0',
+        'djitellopy==1.5'
+    ]
+
+)
