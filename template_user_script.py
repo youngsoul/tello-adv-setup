55e3af88977bd8f59b8a1818d87c78ea196c396c
patrickryan <theyoungsoul@gmail.com>
2020-08-02 21:26:43 -0500

    remove pass from user_template script

--- template_user_script.py
+++ template_user_script.py
@@ -8,9 +8,9 @@ SAMPLE_CONFIG_ITEM = 42
 
 def init(tello, fly_flag=False):
     LOGGER.debug(f"Inside init method.  fly_flag: {fly_flag}, sample config item: {SAMPLE_CONFIG_ITEM}")
-    pass
+
 
 
 def handler(tello, frame, fly_flag=False):
     LOGGER.debug(f"Inside handler method.  fly_flag: {fly_flag}, sample config item: {SAMPLE_CONFIG_ITEM}")
-    pass
+
