69c7cbb0767facf2492d38bf891a8883a5fa9225
patrickryan <theyoungsoul@gmail.com>
2020-08-02 21:03:45 -0500

    update template to use logging not print

--- template_user_script.py
+++ template_user_script.py
@@ -5,9 +5,9 @@ LOGGER = logging.getLogger()
 
 
 def init(tello, fly_flag=False):
-    print(f"Inside init method.  fly_flag: {fly_flag}, sample config item: {SAMPLE_CONFIG_ITEM}")
+    LOGGER.debug(f"Inside init method.  fly_flag: {fly_flag}, sample config item: {SAMPLE_CONFIG_ITEM}")
     pass
 
 def handler(tello, frame, fly_flag=False):
-    print(f"Inside handler method.  fly_flag: {fly_flag}, sample config item: {SAMPLE_CONFIG_ITEM}")
+    LOGGER.debug(f"Inside handler method.  fly_flag: {fly_flag}, sample config item: {SAMPLE_CONFIG_ITEM}")
     pass
